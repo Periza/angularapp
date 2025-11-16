8cdbe674a0ee6d381104655e895bc76f7d89f9b5
Marko Perica <marko.perica2@gmail.com>
2025-11-16 04:52:11 +0100

    add categoryList component

---
+++ src/app/categoryList.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+
+@Component({
+  selector: "category-list",
+  templateUrl: "./categoryListr.component.html"
+})
+export class CategoryList {
+  @Input()
+  selected: string = "";
+
+  @Input()
+  categories: string[] = [];
+
+  @Output()
+  selectCategory = new EventEmitter<string>();
+
+  getBtnClass(category: string): string {
+    return "btn btn-block " + (category === this.selected ? "btn-primary" : "btn-secondary");
+  }
+}
